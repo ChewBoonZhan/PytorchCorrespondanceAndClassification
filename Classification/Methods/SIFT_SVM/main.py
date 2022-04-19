9e93fca40f9de661e9a6030db7c2fb14ddfb8690
xinyeegoh <gohxy282@gmail.com>
2022-04-19 20:24:38 +0800

    naming bugs

--- Classification/Methods/SIFT_SVM/main.py
+++ Classification/Methods/SIFT_SVM/main.py
@@ -2,10 +2,10 @@ import sys
 import os
 
 sys.path.insert(0, os.getcwd() + "/../HOG_SIFT/")
-from sift import sift, evaluate_sift
+from sift_classification import sift, evaluate_sift
 
 if __name__ == '__main__':
     
     #classification using SIFT
-    test_labels, pred_labels, true_classes_sift, predict_classes_sift, image_paths_test = sift()
+    test_labels, pred_labels, true_classes_sift, predict_classes_sift, image_paths_test = sift_extract()
     evaluate_sift(test_labels, pred_labels, true_classes_sift, predict_classes_sift, image_paths_test)
