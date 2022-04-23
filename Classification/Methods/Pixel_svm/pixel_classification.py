ab4626e84a7523638897472a0ae5b5a4c7afc915
HuiFang <huifang00@gmail.com>
2022-04-23 18:47:48 +0800

    fix - change var type to seed_type

--- Classification/Methods/Pixel_svm/pixel_classification.py
+++ Classification/Methods/Pixel_svm/pixel_classification.py
@@ -108,11 +108,11 @@ def evaluate_pixel(pixel_train, pixel_test):
     path_to_results_good_seeds = os.getcwd()+ '/../../../Data/ProcessedData/SIFT_try/Classification_results_Pixel/Good_seeds/' 
 
     seed_set = []
-    type = []
+    seed_type = []
 
     for i in np.array(image_paths_test):
         seed_set.append(i.split('/')[-3].split('S')[1]) # set of seeds
-        type.append(i.split('/')[-4]) # type of seeds
+        seed_type.append(i.split('/')[-4]) # type of seeds
 
-    save_results_corr_image(path_to_results_bad_seeds,path_to_results_good_seeds, len(testData), y_pred, seed_set, type)
+    save_results_corr_image(path_to_results_bad_seeds,path_to_results_good_seeds, len(testData), y_pred, seed_set, seed_type)
     
